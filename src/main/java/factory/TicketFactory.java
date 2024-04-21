5d8dbab6461657f61dcf84c0824506419bd4c30f
Nara Macias <116755949+Lilnnara@users.noreply.github.com>
2024-04-20 21:30:32 -0600

    get revenue works now along with ticket (#34)

--- src/main/java/factory/TicketFactory.java
+++ src/main/java/factory/TicketFactory.java
@@ -6,8 +6,18 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 
+/**
+ * Factory class to create instances of Ticket.
+ * It handles the conversion of attribute values from a HashMap to construct a Ticket object.
+ */
 public class TicketFactory implements Factory<Ticket> {
 
+    /**
+     * Creates a Ticket object from a map of attributes.
+     *
+     * @param attributes A HashMap containing String key-value pairs of ticket attributes.
+     * @return A Ticket object constructed from the provided attributes or null if an error occurs.
+     */
     @Override
     public Ticket create(HashMap<String, String> attributes) {
         try {
@@ -15,15 +25,16 @@ public class TicketFactory implements Factory<Ticket> {
             String userFirstName = attributes.getOrDefault("FirstName", "Unknown");
             String userLastName = attributes.getOrDefault("LastName", "Unknown");
             String username = attributes.getOrDefault("Username", "Unknown");
-            int carId = Integer.parseInt(attributes.getOrDefault("CarID", "0"));
+            String carId = attributes.getOrDefault("CarID", "0"); // Now expecting a String for CarID
             String purchasedCarModel = attributes.getOrDefault("CarModel", "Unknown");
+            String carType = attributes.getOrDefault("CarType", "Unknown"); // Added carType attribute
             int carYear = Integer.parseInt(attributes.getOrDefault("CarYear", "0"));
             double originalPrice = Double.parseDouble(attributes.getOrDefault("OriginalPrice", "0.0"));
             double finalPrice = Double.parseDouble(attributes.getOrDefault("FinalPrice", "0.0"));
             Date purchaseDate = parseDate(attributes.getOrDefault("PurchaseDate", "1970-01-01"));
             boolean isReturn = "Return".equalsIgnoreCase(attributes.getOrDefault("TicketType", "Purchase"));
 
-            return new Ticket(ticketId, userFirstName, userLastName, username, carId, purchasedCarModel, carYear,
+            return new Ticket(ticketId, userFirstName, userLastName, username, carId, purchasedCarModel, carType, carYear,
                               originalPrice, finalPrice, purchaseDate, isReturn);
         } catch (NumberFormatException | ParseException e) {
             System.err.println("Error creating ticket with attributes: " + attributes);
@@ -32,6 +43,13 @@ public class TicketFactory implements Factory<Ticket> {
         }
     }
 
+    /**
+     * Parses a date string into a Date object.
+     *
+     * @param dateStr The date string in "yyyy-MM-dd" format.
+     * @return A Date object representing the specified date.
+     * @throws ParseException if the date string is not in the expected format.
+     */
     private Date parseDate(String dateStr) throws ParseException {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         return sdf.parse(dateStr);
